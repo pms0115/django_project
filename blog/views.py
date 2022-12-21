c418494a7cb4c40ea8aefde754b0fc9ea0ad9d7e
pms0115 <pam2984@naver.com>
2022-12-21 12:51:44 +0900

    Django Project

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_edit(request, pk):
@@ -40,4 +40,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request,'blog/post_edit.html', {'form': form})
+
+
 
